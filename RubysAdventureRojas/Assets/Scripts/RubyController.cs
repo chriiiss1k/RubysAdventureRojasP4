a640e22b743f7662ba647afac3fb7116748e45a5
chriiiss1k <116381048+chriiiss1k@users.noreply.github.com>
2022-11-04 11:44:37 -0700

    added scenes
    added scenes

---
+++ RubysAdventureRojas/Assets/Scripts/RubyController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create 2 variables to use the unity built in axes
+        float horizontial = Input.GetAxis("Horizontal");
+        float vertial = Input.GetAxis("Vertical");
+    
+     //create my movement vector
+     Vector2 position = transform.position;
+
+     //Create the movement of my character
+     position.x = position.x + 3.0f * horizontial* Time.deltaTime;
+     position.y = position.y + 3.0f * vertical* Time.deltaTime;
+
+     //Set the new position
+}    transform.position = position;
